7e967d37ad53ed56d9870bbd0e08039966ef1721
Shubham-Jadhav11 <jadhav.shubham1805@gmail.com>
2022-01-02 16:27:18 +0530

    Added service interfaces Closes #3

---
+++ pet-clinic-data/src/main/java/com/marvel/petclinic/service/VetService.java
@@ -0,0 +1,14 @@
+package com.marvel.petclinic.service;
+
+import com.marvel.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
