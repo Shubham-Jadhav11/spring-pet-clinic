d1c75b70f12e62f1c1c3b07bf44ea979b160e328
Shubham-Jadhav11 <jadhav.shubham1805@gmail.com>
2022-01-12 17:11:02 +0530

    Implemented SpecialityService Closes #26

---
+++ pet-clinic-data/src/main/java/com/marvel/petclinic/service/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.marvel.petclinic.service;
+
+import com.marvel.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/marvel/petclinic/service/map/SpecialityServiceMap.java
@@ -0,0 +1,33 @@
+package com.marvel.petclinic.service.map;
+
+import com.marvel.petclinic.model.Speciality;
+import com.marvel.petclinic.service.SpecialityService;
+
+import java.util.Set;
+
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
