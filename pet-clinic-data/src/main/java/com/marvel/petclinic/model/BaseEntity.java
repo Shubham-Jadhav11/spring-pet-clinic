22cbbc8cad1bceda0a8157ae37c8b61be6bf2368
Shubham-Jadhav11 <jadhav.shubham1805@gmail.com>
2022-01-16 21:30:40 +0530

    Implemented Base Entity (JPA) Closes #30

--- pet-clinic-data/src/main/java/com/marvel/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/marvel/petclinic/model/BaseEntity.java
@@ -1,9 +1,17 @@
 package com.marvel.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
     private final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
