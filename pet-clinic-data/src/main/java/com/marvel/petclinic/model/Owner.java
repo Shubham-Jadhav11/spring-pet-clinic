5a657de2ac785ce295dcd280b8270da317b6c291
Shubham-Jadhav11 <jadhav.shubham1805@gmail.com>
2022-01-17 06:55:09 +0530

    Converted owner to JPA Entity Closes #31

--- pet-clinic-data/src/main/java/com/marvel/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/marvel/petclinic/model/Owner.java
@@ -1,13 +1,28 @@
 package com.marvel.petclinic.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @Column(name = "pet")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pet = new HashSet<>();
 
     public String getAddress() {
